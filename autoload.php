c1b6e78c5a8dc94d57c95f415b547e6cde6306e6
Boris Morel <boris.morel@imag.fr>
2012-06-19 13:59:23 +0200

    Version not working ..

--- autoload.php
+++ autoload.php
@@ -2,6 +2,13 @@
 
 require_once(dirname(__FILE__).'/vendors/ezcomponents/Base/src/base.php');
 
-function __autoload($class) {
-    ezcBase::autoload($class);
+
+function IMAGFile($class) {
+    if (file_exists(dirname(__FILE__).'/vendors/IMAG/'.$class.'.php')) {
+        require_once(dirname(__FILE__).'/vendors/IMAG/'.$class.'.php');
+    }
 }
+
+
+spl_autoload_register("ezcBase::autoload");
+spl_autoload_register("IMAGFile");
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once(dirname(__FILE__).'/autoload.php');
-
-$server = ezcWebdavServer::getInstance();
-$backend = new ezcWebdavFileBackend('/tmp');
-$server->handle( $backend );
